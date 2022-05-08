8f2d8780aacbfab4d0589aa8180400dd414db9d7
billmartillano <francisfrancisbill@gmail.com>
2022-05-08 16:31:38 +0800

    adding my fist song list

---
+++ song1.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>My Personal Blog</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Song List:</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="account.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Mylistsong.php">MyList Song</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="MyHobby.php">My Hobby</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="FavoriteFood.php">Favorite Food</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="ArtistIlike.php">Artist I Like</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="index.php">Back</a></li>
+                    </ul>
+                    
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">My List Song</h1>
+                <p class="lead">"Where words fail, music speaks"</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+                         echo "<h2 class='text-primary'>When I Met You</h2>";
+                         echo "<p class='lead '>Apo Hiking Society</p>";
+
+                         echo "<p>There I was, an empty piece of a shell</p>";
+                         echo "<p>Just minding my own world</p>";
+                         echo "<p>Without even knowing what love and life were all about</p>";
+                         echo "<br/>";
+
+                         echo "<p>Then you came</p>";
+                         echo "<p>You brought me out of the shell</p>";
+                         echo "<p>You gave the world to me</p>";
+                         echo "<p>And before I knew</p>";
+                         echo "<p>There I was, so in love with you</p>";
+                         echo "<br/>";
+
+                         echo "<p>You gave me a reason for my being</p>";
+                         echo "<p>And I love what I'm feeling</p>";
+                         echo "<p>You gave me a meaning to my life</p>";
+                         echo "<p>Yes, I've gone beyond existing</p>";
+                         echo "<p>And it all began when I met you</p>";
+                         echo "<br/>";
+
+                         echo "<p>I love the touch of your hair</p>";
+                         echo "<p>And when I look in your eyes</p>";
+                         echo "<p>I just know, I know I'm on to something good</p>";
+                         echo "<br/>";
+
+                         echo "<p>And I'm sure, my love for you will endure</p>";
+                         echo "<p>Your love will light up my world</p>";
+                         echo "<p>And take all my cares away with the aching part of me</p>";
+                         echo "<br/>";
+
+                         echo "<p>You gave me a reason for my being</p>";
+                         echo "<p>And I love what I'm feeling</p>";
+                         echo "<p>You gave me a meaning to my life</p>";
+                         echo "<p>Yes, I've gone beyond existing</p>";
+                         echo "<p>And it all began when I met you</p>";
+                         echo "<br/>";
+
+                         echo "<p>You taught me how to love</p>";
+                         echo "<p>You showed me a tomorrow and today</p>";
+                         echo "<p>My love, that's different from the yesterday I knew</p>";
+                         echo "<p>You taught me how to love</p>";
+                         echo "<p>And darling, I will always cherish you</p>";
+                         echo "<p>Today, tomorrow, and forever</p>";
+                         echo "<br/>";
+
+                         echo "<p>And I'm sure when evening comes around</p>";
+                         echo "<p>I know, we'll be making love like never before</p>";
+                         echo "<p>My love, who could ask for more?</p>";
+                         echo "<br/>";
+
+                         echo "<p>You gave me a reason for my being</p>";
+                         echo "<p>And I love what I'm feeling</p>";
+                         echo "<br/>";
+
+                         echo "<p>You gave me a meaning to my life</p>";
+                         echo "<p>Yes, I've gone beyond existing</p>";
+                         echo "<p>And it all began when I met you</p>";
+                         echo "<p>When I met you</p>";
+                         echo "<br/>";
+
+                         echo "<p>You gave me a reason (for my being)</p>";
+                         echo "<p>And I love what I'm feeling (love what I'm feeling)</p>";
+                         echo "<p>You gave me a meaning to my life</p>";
+                         echo "<p>Yes, I've gone beyond existing</p>";
+                         echo "<p>And it all began when I met you</p>";
+                         echo "<br/>";
+
+                         echo "<p>When I met you</p>";
+                         echo "<br/>";
+
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
