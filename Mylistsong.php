7ab914c88ebc9b3f8feaf0bd921bd6efc558433f
billmartillano <francisfrancisbill@gmail.com>
2022-05-08 16:29:49 +0800

    adding the final edit of my listsong

---
+++ Mylistsong.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>My Personal Blog</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">Song List:</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="account.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Mylistsong.php">MyList Song</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="MyHobby.php">My Hobby</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="FavoriteFood.php">Favorite Food</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="ArtistIlike.php">Artist I Like</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="index.php">Back</a></li>
+                    </ul>
+                    
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">My List Song</h1>
+                <p class="lead">"Where words fail, music speaks"</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                         <div class="py-4 px-4">
+                <div class="d-flex align-items-center justify-content-between mb-3">
+                    <h5 class="mb-0">Favorite Song:</h5><a href="#" class="btn btn-link text-muted">Show all</a>
+                </div>
+
+                        <h5><a href = "song2.php">1.SunFlower</a></h5>
+                        <br/>
+                        <h5><a href = "song3.php">2.Paraluman</a></h5>
+                        <br/>
+                        <h5><a href = "song4.php">3.Pagsamo</a></h5>
+                        <br/>
+                        <h5><a href = "song5.php">4.You</a></h5>
+                        <br/>
+                        <h5><a href = "song1.php">5.When I Met You</a></h5>
+                        <br/>
+
+
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
