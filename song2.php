7e1c94be22094c30d4220db9248a35ce0a9ccebf
billmartillano <francisfrancisbill@gmail.com>
2022-05-08 16:32:45 +0800

    adding the second songlist

---
+++ song2.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>My Personal Blog</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Song List</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="account.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Mylistsong.php">MyList Song</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="MyHobby.php">My Hobby</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="FavoriteFood.php">Favorite Food</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="ArtistIlike.php">Artist I Like</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="index.php">Back</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">My List Song</h1>
+                <p class="lead">"Where words fail, music speaks"</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+                         echo "<h2 class='text-primary'>Sunflower</h2>";
+                         echo "<p class='lead '>Post Malone and Swae Lee</p>";
+
+                         echo "<p>Ayy, ayy, ayy, ayy (ooh)</p>";
+                         echo "<p>Ooh, ooh, ooh, ooh (ooh)</p>";
+                         echo "<p>Ayy, ayy</p>";
+                         echo "<p>Ooh, ooh, ooh, ooh</p>";
+                         echo "<br/>";
+
+                         echo "<p>Needless to say, I keep her in check</p>";
+                         echo "<p>She was a bad-bad, nevertheless (yeah)</p>";
+                         echo "<p>Callin' it quits now, baby, I'm a wreck (wreck)</p>";
+                         echo "<p>Crash at my place, baby, you're a wreck (wreck)</p>";
+                         echo "<br/>";
+
+                         echo "<p>Needless to say, I'm keeping her in check</p>";
+                         echo "<p>She was all bad-bad, nevertheless</p>";
+                         echo "<p>Callin' it quits now, baby, I'm a wreck</p>";
+                         echo "<p>Crash at my place, baby, you're a wreck</p>";
+                         echo "<br/>";
+
+                         echo "<p>Thinkin' in a bad way, losin' your grip</p>";
+                         echo "<p>Screamin' at my face, baby, don't trip</p>";
+                         echo "<p>Someone took a big L, don't know how that felt</p>";
+                         echo "<p>Lookin' at you sideways, party on tilt</p>";
+                         echo "<br/>";
+
+                         echo "<p>Ooh-ooh-ooh</p>";
+                         echo "<p>Some things you just can't refuse</p>";
+                         echo "<p>She wanna ride me like a cruise</p>";
+                         echo "<p>And I'm not tryna lose</p>";
+                         echo "<br/>";
+
+                         echo "<p>Then you're left in the dust</p>";
+                         echo "<p>Unless I stuck by ya</p>";
+                         echo "<p>You're the sunflower</p>";
+                         echo "<p>I think your love would be too much</p>";
+                         echo "<p>Or you'll be left in the dust</p>";
+                         echo "<p>Unless I stuck by ya</p>";
+                         echo "<p>You're the sunflower</p>";
+                         echo "<p>You're the sunflower</p>";
+                         echo "<br/>";
+
+                         echo "<p>Every time I'm leavin' on you (ooh)</p>";
+                         echo "<p>You don't make it easy, no (no, no)</p>";
+                         echo "<p>Wish I could be there for you</p>";
+                         echo "<p>Give me a reason to, oh (oh)</p>";
+                         echo "<br/>";
+
+                         echo "<p>Every time I'm walkin' out</p>";
+                         echo "<p>I can hear you tellin' me to turn around</p>";
+                         echo "<p>Fightin' for my trust and you won't back down</p>";
+                         echo "<p>Even if we gotta risk it all right now, oh (now)</p>";
+                         echo "<br/>";
+
+                         echo "<p>I know you're scared of the unknown ('known)</p>";
+                         echo "<p>You don't wanna be alone (alone)</p>";
+                         echo "<p>I know I always come and go (and go)</p>";
+                         echo "<p>But it's out of my control</p>";
+                         echo "<br/>";
+
+                         echo "<p>And you'll be left in the dust</p>";
+                         echo "<p>Unless I stuck by ya</p>";
+                         echo "<p>You're the sunflower</p>";
+                         echo "<p>I think your love would be too much</p>";
+                         echo "<p>Or you'll be left in the dust</p>";
+                         echo "<p>Unless I stuck by ya</p>";
+                         echo "<p>You're the sunflower</p>";
+                         echo "<p>You're the sunflower (yeah)</p>";
+                         echo "<br/>";
+
+                           
+                        ?>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
